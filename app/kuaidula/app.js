92921766abfe2811a526716eb9bb1d6a826b04ad
aaHung <landxh@gmail.com>
2014-12-05 11:25:07 +0800

    add article class and Q & mysql module. set up routes. set up example output of service (or empty)

--- app/kuaidula/app.js
+++ app/kuaidula/app.js
@@ -6,10 +6,6 @@ var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -19,9 +15,12 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    if (app.get('env') === 'development') {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    } else
+        res.redirect('/');
 });
 
 // error handlers
--- app/kuaidula/routes/index.js
+++ app/kuaidula/routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.end('Welcome to kuaidula.com');
+    res.end('Welcome to kuaidula.com');
+});
+
+// get detail of article
+router.get('/articles/:id', function(req, res) {
+    var Article = require('../class/article.js');
+    var article = new Article(req.params.id);
+    article.get();
+    res.end(JSON.stringify(article));
+});
+
+// get article list
+router.get('/articles/', function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    var articles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    res.end(JSON.stringify(articles));
 });
 
 module.exports = router;
